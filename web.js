4b6837b015108f2dfd00f8e513c7aa8b446283e2
Ubuntu <ubuntu@ip-172-31-22-47.us-west-2.compute.internal>
2013-07-13 13:47:42 +0000

    tweaked web.js

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 fs.readFile('index.html', function(err, data) {
-  indexfile = data.toString()
-
+  indexfile = data.toString();
+  buf = new Buffer(indexfile, "utf-8");
   if (err) throw err;
   app.get('/', function(request, response) {
-    response.send(indexfile);
+    response.send(buf);
   });
 });
 
